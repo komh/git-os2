28c0d8f0cb070652b55b92d4d28fd904ab428bd6
KO Myung-Hun <komh@chollian.net>
2012-03-24 15:57:28 +0900

    Fix the problem that git-push fails due to 'Disk full'

--- wrapper.c
+++ wrapper.c
@@ -155,10 +155,21 @@ ssize_t xread(int fd, void *buf, size_t len)
 ssize_t xwrite(int fd, const void *buf, size_t len)
 {
 	ssize_t nr;
+#if defined(__OS2__) && defined(__KLIBC__)
+	int pipehandle = 0;
+	struct stat statbuf;
+
+	if (!fstat(fd, &statbuf))
+		pipehandle = S_ISFIFO(statbuf.st_mode);
+#endif
 	while (1) {
 		nr = write(fd, buf, len);
 		if ((nr < 0) && (errno == EAGAIN || errno == EINTR))
 			continue;
+#if defined(__OS2__) && defined(__KLIBC__)
+		if ((nr < 0) && pipehandle && (errno == ENOSPC))
+			continue;
+#endif
 		return nr;
 	}
 }
