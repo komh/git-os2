e64c649b965245bb9abac68f620fdfd2b08ab566
KO Myung-Hun <komh@chollian.net>
2015-09-13 16:28:14 +0900

    Disable chmod() on a config lock file on OS/2
    On OS/2, chmod() fails on an opened file. This should be fixed later.
    
        modified:   config.c

--- config.c
+++ config.c
@@ -2086,12 +2086,15 @@ int git_config_set_multivar_in_file(const char *config_filename,
 		close(in_fd);
 		in_fd = -1;
 
+		/* FIXME: On OS/2, chmod() does not work on a opened file. */
+#ifndef __OS2__
 		if (chmod(get_lock_file_path(lock), st.st_mode & 07777) < 0) {
 			error("chmod on %s failed: %s",
 			      get_lock_file_path(lock), strerror(errno));
 			ret = CONFIG_NO_WRITE;
 			goto out_free;
 		}
+#endif
 
 		if (store.seen == 0)
 			store.seen = 1;
@@ -2275,11 +2278,14 @@ int git_config_rename_section_in_file(const char *config_filename,
 
 	fstat(fileno(config_file), &st);
 
+	/* FIXME: On OS/2, chmod() does not work on a opened file. */
+#ifndef __OS2__
 	if (chmod(get_lock_file_path(lock), st.st_mode & 07777) < 0) {
 		ret = error("chmod on %s failed: %s",
 			    get_lock_file_path(lock), strerror(errno));
 		goto out;
 	}
+#endif
 
 	while (fgets(buf, sizeof(buf), config_file)) {
 		int i;
