13b67b31d38fd06f1bcb34c55e4d7220c203af5a
KO Myung-Hun <komh@chollian.net>
2017-01-17 11:00:26 +0900

    Use common build process
        modified:   config.mak.uname
        new file:   configure.cmd

---
+++ configure.cmd
@@ -0,0 +1,20 @@
+extproc sh
+
+d=$(dirname "$0" | tr '\\' /)
+
+n=configure
+test -f "$d/$n." || { echo "\`$d/$n' not found !!!"; exit 1; }
+
+opts="
+    --enable-pthreads
+    --with-shell=sh
+    --with-perl=perl
+    --with-python=python
+    --with-curl
+    --with-openssl
+    --with-expat
+    --without-tcltk
+    LDFLAGS='-Zomf -Zbin-files -Zhigh-mem'
+"
+
+eval "$d/$n" $opts "$@" 2>&1 | tee "$n.log"
