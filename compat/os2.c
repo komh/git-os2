880e45e9ee4863730081a54eab0f3c636313edef
KO Myung-Hun <komh@chollian.net>
2017-07-21 18:46:23 +0900

    Implement skip_dos_drive_prefix() and offset_1st_component()
        modified:   compat/os2-git-compat.h
        modified:   compat/os2.c

--- compat/os2.c
+++ compat/os2.c
@@ -1741,6 +1741,26 @@ void *realloc (void *mem, size_t size)
     return ptr ? ptr : _lrealloc (mem, size);
 }
 
+int git_os2_offset_1st_component(const char *path)
+{
+	char *pos = (char *)path;
+
+	/* unc paths */
+	if (!skip_dos_drive_prefix(&pos) &&
+			is_dir_sep(pos[0]) && is_dir_sep(pos[1])) {
+		/* skip server name */
+		pos = strpbrk(pos + 2, "\\/");
+		if (!pos)
+			return 0; /* Error: malformed unc path */
+
+		do {
+			pos++;
+		} while (*pos && !is_dir_sep(*pos));
+	}
+
+	return pos + is_dir_sep(*pos) - path;
+}
+
 /*
 todo:
   + test server commands.
